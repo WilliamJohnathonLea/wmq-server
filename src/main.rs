3212d90f97ab43d8acf1723b9afad1e041547d7a
William Lea <wjlea93@proton.me>
2025-03-16 13:14:22 +0000

    parsing inbound messages and responding

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
+mod messages;
+
 use std::{env, error::Error};
 
+use messages::MsgIn;
 use tokio::{
-    io::AsyncReadExt,
+    io::{AsyncReadExt, AsyncWriteExt},
     net::{TcpListener, TcpStream},
 };
+
+const BUFF_SIZE: usize = 512 * 1024;
+const ACK: &str = "ACK";
+const NACK: &str = "NACK";
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -19,19 +26,27 @@ async fn main() -> Result<(), Box<dyn Error>> {
 }
 
 async fn handle_incoming(mut stream: TcpStream) {
-    let mut in_buffer = [0; 1024];
+    let mut in_buffer = [0; BUFF_SIZE];
 
     loop {
         let n_bytes = stream.read(&mut in_buffer).await;
         match n_bytes {
-            Ok(0) => {
-                println!("socket is closed");
-                return;
-            } // socket closed
-            Ok(n) => println!("read {n} bytes"),
             Err(_) => {
                 eprintln!("failed to read from TCP socket");
                 return;
+            }
+            Ok(0) => {
+                println!("socket is closed");
+                return;
+            }
+            Ok(n) => {
+                println!("read {n} bytes");
+                let Ok(_msg_in) = serde_json::from_slice::<MsgIn>(&in_buffer[..n]) else {
+                    let _ = stream.write(NACK.as_bytes()).await;
+                    continue;
+                };
+
+                let _ = stream.write(ACK.as_bytes()).await;
             }
         }
     }
